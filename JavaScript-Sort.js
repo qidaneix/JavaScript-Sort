6e27c0f9d6d71e87bf45ccc658fbd2982f3de5d7
qidanei <xiaojin1992@126.com>
2016-09-30 08:59:14 +0800

    插入排序，希尔排序，冒泡排序，选择排序

--- JavaScript-Sort.js
+++ JavaScript-Sort.js
@@ -4,7 +4,7 @@
 Array.prototype.swap = function (i, j) {
     var tmp = this[i];
     this[i] = this[j];
-    this[j] = this[i];
+    this[j] = tmp;
 };
 
 //冒泡算法
@@ -14,8 +14,50 @@ Array.prototype.bubbleSort = function () {
             if(this[j] > this[j+1]) {
                 this.swap(j, j+1);
             }
-        };
-    };  
+        }
+    } 
 };
 
-//
+//选择排序
+Array.prototype.selectionSort = function () {
+    for (var i = 0; i < this.length; i++) {
+        var index = i;
+        for (var j = i+1; j < this.length; j++) {
+            if(this[j] < this[index]){
+                index = j;
+            }
+        }
+        this.swap(i, index);
+    }
+};
+
+//插入排序
+Array.prototype.insertionSort = function () {
+    for (var i = 1; i < this.length; i++) {
+        var j = i, value = this[i];
+        while(j > 0 && value < this[j-1]){
+            this[j] = this[j-1];
+            j--;
+        }
+        this[j] = value;
+    }
+};
+
+//希尔排序
+Array.prototype.shellSort = function () {
+    for (var step = parseInt(this.length)/2; step > 0; step--) {
+        for(var i = 0; i < step; i++) {
+            for (var j = step+i; j < this.length; j += step) {
+                var k = j, value = this[k];
+                while(k >= step && value < this[k-step]){
+                    this[k] = this[k-step];
+                    k -= step;
+                }
+                this[k] = value;
+            }
+        }
+    }
+};
+arr = [3,8,7,4,1,9,0,2,5,6];
+arr.shellSort();
+console.log(arr);
